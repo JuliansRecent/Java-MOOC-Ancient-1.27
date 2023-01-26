a11c2a63e8084d3db2a2ebfec656d337ff50c688
JuliansRecent <juliansrecent@gmail.com>
2023-01-25 22:16:39 -0800

    Module complete. Prompt the user for a year, if the year is sooner than 2015, print 'Ancient history!'

--- src/main/java/Ancient.java
+++ src/main/java/Ancient.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 public class Ancient {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        // Prompt the user for a year
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scanner.nextLine());
+        // If the number is smaller than 2015, print Ancient history!
+        if (year < 2015) {
+            System.out.println("Ancient history!");
+        }
     }
 }
